8d09242375ae7ec61fe7432f1e8d12c3a0bff693
devangs581-gif <devangs581@gmail.com>
2026-04-02 21:50:21 +0530

    Update useGif.js

--- src/Hooks/useGif.js
+++ src/Hooks/useGif.js
@@ -1,22 +1,27 @@
 import axios from 'axios';
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import myGif from "../Assets/gif2.gif";
 
 export default function useGif(input) {
 
+  const randomUrl = `https://api.giphy.com/v1/stickers/random?api_key=aILe9ciZJxftAx1MnYsYglxKRrUlOEJ5&rating=g`;
+
+  const tagUrl = `https://api.giphy.com/v1/stickers/random?api_key=aILe9ciZJxftAx1MnYsYglxKRrUlOEJ5&tag=${input}&rating=g`;
+
   const [gif, setGif] = useState(null);
   const [loader, setLoader] = useState(false);
 
-  const fetchData = useCallback(async (tag) => {
+  async function fetchData(tag) {
     setLoader(true);
 
-    const url = tag
-      ? `https://api.giphy.com/v1/stickers/random?api_key=aILe9ciZJxftAx1MnYsYglxKRrUlOEJ5&tag=${tag}&rating=g`
-      : `https://api.giphy.com/v1/stickers/random?api_key=aILe9ciZJxftAx1MnYsYglxKRrUlOEJ5&rating=g`;
+    const url = tag ? tagUrl : randomUrl;
 
     try {
       const { data } = await axios.get(url);
-      const output = data.data.images.fixed_height.url;
+
+      // ✅ use smaller gif for faster loading
+      const output = data.data.images.fixed_height_small.url;
+
       setGif(output || "");
     } catch (e) {
       console.log("ERROR", e);
@@ -24,11 +29,12 @@ export default function useGif(input) {
     }
 
     setLoader(false);
-  }, []);
+  }
 
   useEffect(() => {
     fetchData(input);
-  }, [fetchData, input]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [input]);
 
   return { gif, loader, fetchData };
 }
